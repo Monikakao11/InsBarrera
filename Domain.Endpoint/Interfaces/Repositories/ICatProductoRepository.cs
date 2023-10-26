1f0a8a25883e67c8abcd2ca8dca79aac26ccadfe
BalDiego <148025505+BalDiego@users.noreply.github.com>
2023-10-25 23:52:18 -0600

    Update ICatProductoRepository.cs

--- Domain.Endpoint/Interfaces/Repositories/ICatProductoRepository.cs
+++ Domain.Endpoint/Interfaces/Repositories/ICatProductoRepository.cs
@@ -1,18 +1,18 @@
 ﻿using Domain.Endpoint.Entities;
 using System;
 using System.Collections.Generic;
-
+///Trabajado por Diego Baltodano Octubre 2023 :D
 namespace Domain.Endpoint.Interfaces.Repositories
-{
-    public interface ICatProductoRepository
+public interface ICatProductoRepository
     {
-        List<CatProducto> Get();
+        Task<List<CatProducto>> Get();
 
-        void Create(CatProducto nuevoCatProducto);
 
-        void Delete(Guid Id);
+       void CreateCatProducto(CatProducto nuevoCatProducto);
+        
+        void DeleteCatProducto(Guid Id);
 
-        void UpdateCatProducto(Guid Id, CatProducto nuevosRegistros);
+      void UpdateCatProducto(Guid Id, CatProducto nuevosRegistros);
        
     }
 }
